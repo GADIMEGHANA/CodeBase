118b7461262201b680f638ea23b5bf520d4ac5f5
Gadi Meghana <meghanagadi299@gmail.com>
2024-07-19 19:26:51 +0530

    Rat Houses

--- RatCountHouse.java
+++ RatCountHouse.java
@@ -2,33 +2,38 @@ import java.util.*;
 public class RatCountHouse {
   
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s=sc.next();
-        sc.close();
-        char[] ch=s.toCharArray();
-        if(s.length()==0){
-            System.out.println(-1);
-        }
-        else{
-            char c=0;
-            for(int i=0;i<ch.length-2;i=i+2){
-                c=ch[i];
-                if(ch[i+1]=='A'){
-                    c=(c=='1' && ch[i+2]=='1')? '1':'0';
-                    ch[i+2]=c;
-                   
-                }
-                if(ch[i+1]=='B'){
-                    c=(c=='0' && ch[i+2]=='0')?'0':'1';
-                    ch[i+2]=c;
-                }
-                if(ch[i+1]=='C'){
-                    c=((c=='1' && ch[i+2]=='1') || (c=='0' && ch[i+2]=='0'))?'0':'1';
-                    ch[i+2]=c;
-                }
-            }
-            System.out.println(c);
-        }
-    }
-}
+   public static void main(String[] args) {
+          System.out.println("Try programiz.pro");
+          Scanner sc=new Scanner(System.in);
+          int r=sc.nextInt();
+          int n=sc.nextInt();
+          int[] a=new int[n];
+          for(int i=0;i<n;i++){
+              a[i]=sc.nextInt();
+          }
+          sc.close();
+          int l=0;
+          int co=0;
+          if(n==0){
+              System.out.println(-1);
+          }
+          else{
+          for(int i=0;i<n;i++){
+            l+=a[i];
+           co+=1;
+           if(l>r*2){
+               System.out.println(co);
+                   break;
+           }
+           if(l<r*2){
+              System.out.println(0);
+          }
+           }
+          }
+         
+         
+         
+      }
+  }
+  
+        
